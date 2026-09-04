575fa57f744cbbf11d6d94cf67b0649eb663af59
tareq-stonybrook <tareq.mahmud@stonybrook.edu>
2026-09-04 01:09:07 -0400

    2nd commit, added monthly and yearly salary

--- salary.py
+++ salary.py
@@ -7,5 +7,7 @@ take_home_pay = gross_pay - tax_amount
 if gross_pay > 0:
     print("total pay:",take_home_pay)
     print("bi-weekly pay:", take_home_pay *2)
+    print("monthly pay:", take_home_pay * 4)
+    print("yearly pay:", take_home_pay * 52)
 if gross_pay <= 0:
     print("total pay must be greater than 0")
