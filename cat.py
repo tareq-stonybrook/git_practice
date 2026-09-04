00eba77a15d7296cc709f5d64ef5d1947422473e
tareq-stonybrook <tareq.mahmud@stonybrook.edu>
2026-09-03 23:46:46 -0400

    1st commit

---
+++ cat.py
@@ -0,0 +1,9 @@
+score=[23,32,13,45,67,89,12,34,56,78]
+x=sum(score)
+y = len(score)
+average = x / y
+z=max(score)
+g=min(score)
+print("The average score is:", average)
+print("The highest score is:", z)
+print("The lowest score is:", g)
